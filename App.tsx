b3487a935aa58a540bff4a352c40415786be4aee
Tomas Cuevas <memo_mister398@hotmail.com>
2023-06-15 14:48:45 -0300

    change StatusBar color

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { NativeRouter } from "react-router-native";
+import { StatusBar } from "expo-status-bar";
 
 //* MAIN-COMPONENT *//
 import { Main } from "./src/components/Main";
 
 export default function App() {
   return (
-    <NativeRouter>
-      <Main />
-    </NativeRouter>
+    <>
+      <StatusBar style="light" />
+      <NativeRouter>
+        <Main />
+      </NativeRouter>
+    </>
   );
 }
