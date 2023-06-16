428b6891c7cb4b0fc82c8d51b370778262edc7cd
Tomas Cuevas <memo_mister398@hotmail.com>
2023-06-16 10:16:01 -0300

    global.fetch y globalThis

--- packages/client/src/components/RepositoryList.tsx
+++ packages/client/src/components/RepositoryList.tsx
@@ -11,7 +11,9 @@ export const RepositoryList = () => {
   const [repositories, setRepositories] = useState<IRepository[]>();
 
   const getRepository = async () => {
-    const response = await fetch("http://192.168.1.11:5000/api/repositories");
+    const response = await globalThis.fetch(
+      "http://192.168.1.11:5000/api/repositories"
+    );
     const data: IData = await response.json();
     const repositories = data.edges.map((repository) => repository.node);
     setRepositories(repositories);
